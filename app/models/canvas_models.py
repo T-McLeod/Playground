a9cd33a6bd267a2a206899717d6062d196f10a17
Tanner McLeod <TannerMcLeod21@gmail.com>
2025-11-19 10:56:21 -0500

    Add quiz creation functionality with draft and question handling

---
+++ app/models/canvas_models.py
@@ -0,0 +1,39 @@
+
+
+from typing import Dict, List
+
+
+class Quiz_Answer():
+    """
+    Represents an answer option for a Canvas quiz question.
+    """
+    text: str
+    weight: int
+
+    def __init__(self, text: str, weight: int):
+        self.text = text
+        self.weight =   weight
+
+
+    def as_json(self):
+        return {
+            "answer_text": self.text,
+            "answer_weight": self.weight
+        }
+    
+
+class Quiz_Question():
+    """
+    Represents a question in a Canvas quiz.
+    """
+    question_type: str
+    question_text: str
+    points_possible: float
+    answers: List[Quiz_Answer]
+
+    def __init__(self, question_type: str, question_text: str, points_possible: float, answers: List[Quiz_Answer]):
+        self.id = id
+        self.question_type = question_type
+        self.question_text = question_text
+        self.points_possible = points_possible
+        self.answers = answers
