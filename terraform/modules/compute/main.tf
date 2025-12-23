76061719624aa20cd5cabc52dc8762c790c658ad
Tanner McLeod <TannerMcLeod21@gmail.com>
2025-12-23 14:10:48 -0500

    Add IAM member for public access to Cloud Run service and output service URL

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -32,3 +32,10 @@ resource "google_cloud_run_v2_service" "app" {
     }
   }
 }
+
+resource "google_cloud_run_service_iam_member" "public_access" {
+  location = google_cloud_run_v2_service.app.location
+  service  = google_cloud_run_v2_service.app.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
