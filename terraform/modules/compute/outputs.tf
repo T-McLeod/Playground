c3ea7ca3226036f70104b8d038723124524db61e
Tanner McLeod <TannerMcLeod21@gmail.com>
2025-12-26 21:40:56 -0500

    Add artifact registry outputs and update Docker image path in workflow

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -1,3 +1,13 @@
 output "service_url" {
   value = google_cloud_run_v2_service.app.uri
 }
+
+output "artifact_registry_repo" {
+  value = google_artifact_registry_repository.playground-tf-repo.repository_id
+}
+
+output "artifact_registry_repo_path" {
+  description = "The base path for the Docker repository"
+  # This constructs the path: us-east1-docker.pkg.dev/playground-ai-478208/playground-global-backend-repo
+  value = "${google_artifact_registry_repository.playground-tf-repo.location}-docker.pkg.dev/${google_artifact_registry_repository.playground-tf-repo.project}/${google_artifact_registry_repository.playground-tf-repo.repository_id}"
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,7 @@
+output "service_url" {
+  value = module.compute.service_url
+}
+
+output "artifact_registry_repo_path" {
+  value = module.compute.artifact_registry_repo_path
+}
